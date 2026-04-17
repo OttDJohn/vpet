07dc6cd51fce2c0fa57a181da2e82d00043bb57d
Danny Wilkins <tekk@sdf.org>
2026-04-17 00:52:30 -0400

    New checkpoint. Practically rewrote frontend

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 let images = {};
 
 async function load() {
-	let i = ["cornelius", "meatfull", "meatempty", "barbelfull", "barbelempty", "grave"];
+	let i = ["cornelius", "corneliusright", "meatfull", "meatempty", "trainfull", "trainempty", "grave", "graveright"];
 	let p = [];
 	i.forEach(function(s) {
 			p.push(new Promise(resolve => {
@@ -24,8 +24,6 @@ let pet = {
 };
 
 let stor = null;
-let w = null;
-let c = null;
 let laf = null;
 
 function roll() {
@@ -35,59 +33,44 @@ function roll() {
 
 function flip() {
 		pet.right = !pet.right;
-}
-
-function drawBg() {
-	// Sky
-	let g = c.createRadialGradient(160, 212, 16, 160, 212, 140);
-	g.addColorStop(0, "orange");
-	g.addColorStop(0.3, "darkorange");
-	g.addColorStop(1, "purple");
-	c.fillStyle = g;
-	c.fillRect(0, 0, 240, 212);
-	// Ground
-	g = c.createLinearGradient(120, 212, 120, 340);
-	g.addColorStop(0, "darkgreen");
-	g.addColorStop(1, "lightgreen");
-	c.fillStyle = g;
-	c.fillRect(0, 212, 240, 108);
+		document.getElementById("pet").src = pet.right ? images.corneliusright.src : images.cornelius.src;
 }
 
 function drawStatus() {
-	let xbase = 3;
-	for (let i = 0; i < 5; i++) {
-		c.drawImage(i < pet.food ? images.meatfull : images.meatempty, xbase + i * 25, 3);
-		c.drawImage(i < pet.train ? images.barbelfull : images.barbelempty, xbase + i * 25, 30);
-	}
+	let i=0;
+	document.querySelectorAll(".meat").forEach((e) =>
+		e.src = i++ < pet.food ? images.meatfull.src : images.meatempty.src
+	);
+	i = 0;
+	document.querySelectorAll(".train").forEach((e) =>
+		e.src = i++ < pet.train ? images.trainfull.src : images.trainempty.src
+	);
 }
 
 function animTick(ts) {
-	c.clearRect(0, 0, w.width, w.height);
 	let df = pet.right ? 1 : -1;
-	if (!pet.dead && ts - laf > 100) {
-		laf = ts;
-		
-		if (!pet.right && pet.x <= 0 || pet.right && pet.x >= w.clientWidth - 51 || roll() < 3) {
-			flip();
-			df = pet.right ? 1 : -1;
+	if (!pet.dead) {
+		if (ts - laf > 100) {
+			laf = ts;
+			let w = document.getElementById("field");
+			let p = document.getElementById("pet");
+			if (!pet.right && pet.x <= 0 || pet.right && pet.x >= field.clientWidth - 51 || roll() < 3) {
+				flip();
+				df = pet.right ? 1 : -1;
+			}
+			if (roll() < 30) {
+				pet.x += 5 * df;
+				p.style.left = pet.x + "px";
+			}
 		}
-		if (roll() < 30) {
-			pet.x += 5 * df;
-		}
+		window.requestAnimationFrame(animTick);
 	}
-	drawBg();
-	drawStatus();
-	c.save();
-	c.scale(df, 1);
-	c.drawImage(pet.i, 0, 0, 48, 49, df * pet.x, w.height - 48, 48 * df, 49);
-	c.restore();
-	window.requestAnimationFrame(animTick);
 }
 
 function simTick() {
 	if (pet.dead || pet.food <= -5 || pet.train <= -5) {
 		pet.dead = true;
-		pet.i = images.grave;
+		document.getElementById("pet").src = pet.left ? images.grave.src : images.graveright.src;
 		return;
 	}
 	if (roll() < 5) {
@@ -97,6 +80,7 @@ function simTick() {
 	if (roll() < 8) {
 		pet.train--;
 	}
+	drawStatus();
 }
 
 function clamp(min, mid, max) {
@@ -118,19 +102,16 @@ function keyDown(e) {
 
 window.addEventListener("load", function() {
 	stor = window.localStorage;
-	w = document.getElementById("game");
-	c = w.getContext("2d");
 	let save = stor.getItem("pet");
 	if (save !== null) {
 		pet = save;
 	}
-	setInterval(simTick, 60 * 10);
 	load().then(_ => {
-		pet.i = images.cornelius;
 		laf = document.timeline.currentTime;
 		document.getElementById("softkey-left").onclick = () => { pet.food = clamp(1, pet.food + 1, 5)};
 		document.getElementById("softkey-right").onclick = () => { pet.train = clamp(1, pet.train + 1, 5)};
 		document.addEventListener('keydown', keyDown);
 		window.requestAnimationFrame(animTick);
+		setInterval(simTick, 60 * 10);
 	});
 });
