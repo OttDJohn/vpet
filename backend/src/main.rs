6963a46a377d216bd33915ab3628b2829ce50e8c
Danny Wilkins <tekk@sdf.org>
2026-04-25 11:43:45 -0400

    A minimalist but working backend (not interactive yet)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,42 +1,125 @@
+mod sim;
+
+use sim::pet::{Pet, PetType, tick};
 use std::fs::DirBuilder;
-use std::path;
+use std::sync::{Arc, LazyLock};
+use std::thread;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use const_format::concatcp;
 
 use heed::{Database, Env, EnvOpenOptions};
 use heed::types::*;
 
+use ohkami::prelude::*;
 use ohkami::{Ohkami, Route};
 use ohkami::claw::{Json, Path};
 use ohkami::fang::Context;
-use ohkami::serde::{Deserialize, Serialize};
+use ohkami::serde::{Serialize};
+
+use serde_json;
 
 use uuid::Uuid;
 
 const API_PREFIX: &'static str = "/api/v1/";
 
 #[derive(Serialize)]
-struct PetResult {
+struct CreatePetResult {
 	id: Uuid,
 }
 
-async fn create_pet(Context(dbenv): Context<'_, Env>) -> Json<PetResult> {
+
+#[derive(Serialize)]
+struct WirePet {
+	t: PetType,
+	food: i8,
+	training: i8,
+	age: u64
+}
+
+#[derive(Serialize)]
+struct Error {
+    status_code: u16,
+    message:     String,
+}
+
+impl IntoResponse for Error {
+	fn into_response(self) -> Response {
+		Response::new(Status::from(self.status_code)).with_json(self)
+	}
+}
+impl From<uuid::Error> for Error {
+	fn from(_e: uuid::Error) -> Error {
+		Error { status_code: 400, message: String::from("Invalid uuid") }
+	}
+}
+
+fn wire_of_pet(p: Pet) -> WirePet {
+	WirePet {t: p.t, food: p.food, training: p.training, age: (now_s() - p.born) / (60 * 60 * 24) }
+}
+
+async fn create_pet(Context(dbenv): Context<'_, Arc<Env>>) -> Json<CreatePetResult> {
 	let u = Uuid::new_v4();
 	let mut wtx = dbenv.write_txn().expect("Failed to open write transaction");
 	let pets: Database<Str, Str> = dbenv.create_database(&mut wtx, Some("pets"))
 		.expect("Couldn't open pets db");
-	pets.put(&mut wtx, &u.to_string(), "exists");
+	_ = pets.put(&mut wtx, &u.to_string(), &(serde_json::to_string(&Pet {t: PetType::Squid, food: 5, training: 5, metabolism: 80, discipline: 80, born: now_s(), alive: true}).unwrap()));
 	wtx.commit().expect("Failed to commit new pet");
 
-	Json(PetResult { id: u })
+	Json(CreatePetResult { id: u })
 }
 
+async fn get_pet(Context(dbenv): Context<'_, Arc<Env>>, Path(id): Path<&str>) -> Result<Json<WirePet>, Error> {
+	let _u = Uuid::parse_str(id)?; // Make sure it's a valid uuid
+
+	let mut rtx = dbenv.read_txn().expect("Failed to open read transaction");
+	let pets: Database<Str, Str> = dbenv.open_database(&mut rtx, Some("pets")).or_else(|_x| Err(Error {status_code: 404, message: String::from("Pet not found!")}))?.expect("Broken database when calling get_pet");
+	if let Ok(pet) = pets.get(&rtx, id) {
+		match pet {
+			Some(pet) => Ok(Json(wire_of_pet(serde_json::from_str(pet).unwrap()))),
+			None => Err(Error {status_code: 404, message: String::from("Invalid id")})
+		}
+	} else {
+		Err(Error {status_code: 500, message: String::from("Error getting from db")})
+	}
+}
+
+fn now_s() -> u64 {
+	SystemTime::now().duration_since(UNIX_EPOCH).expect("Something bad happened with time").as_secs()
+}
+
+fn tick_pet_thread(dbenv: Arc<Env>) {
+	loop {
+		eprintln!("Tick!");
+		let mut rng = rand::rng();
+		let mut wtx = (*dbenv).write_txn().expect("Failed to open pet tick transaction");
+		if let Ok(pets) = dbenv.create_database::<Str,Str>(&mut wtx, Some("pets")) {
+			let iter = pets.iter_mut(&mut wtx);
+			if let Ok(mut ri) = iter {
+				while let Some(val) = ri.next() {
+					let v: (&str, &str) = val.unwrap();
+					let rv = v.1;
+					let k = v.0;
+					let mut p: Pet = serde_json::from_str(rv).unwrap();
+					tick(&mut rng, &mut p);
+					unsafe {_ = ri.put_current(&k, &serde_json::to_string(&p).unwrap())};
+				}
+			}
+		} else {
+			panic!("Failed to open database for ticks");
+		}
+		wtx.commit().expect("Failed to commit pet ticks");
+		thread::sleep(Duration::from_millis(60 * 1000));
+	}
+}
+	
 #[tokio::main]
 async fn main() {
-	let path = path::Path::new("./petdb");
-	DirBuilder::new().recursive(true).create(path).expect("Unable to create db path");
-	let env = unsafe { EnvOpenOptions::new().max_dbs(8).open(path).expect("Failed to open db") };
-
-	Ohkami::new((Context::new(env),
-		concatcp!(API_PREFIX, "pet").POST(create_pet))).howl("localhost:8080").await
+	DirBuilder::new().recursive(true).create("./petdb").expect("Unable to create db path");
+	static ENV: LazyLock<Arc<Env>> = unsafe { std::sync::LazyLock::new(|| {Arc::new(EnvOpenOptions::new().max_dbs(8).open("./petdb").expect("Failed to open db"))}) };
+	thread::spawn(|| {tick_pet_thread((*ENV).clone())});
+	Ohkami::new((Context::new((*ENV).clone()),
+		concatcp!(API_PREFIX, "pet").POST(create_pet),
+		concatcp!(API_PREFIX, "pet/:id").GET(get_pet),
+		)).howl("localhost:8080").await
 }
--- backend/src/sim/pet.rs
+++ backend/src/sim/pet.rs
@@ -3,25 +3,31 @@ use rand::prelude::*;
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Pet {
-	age: u8,
-	food: i8,
-	metab: u8,
-	fun: i8,
-	atten: u8,
-	// At first we want to just delete the entry when we die but we
-	// might want to reincarnate later.
-	alive: bool,
+pub enum PetType {
+	Squid
 }
 
-fn tick(rng: &Rng, p: &mut Pet) {
-	if (rng::random_range(0..100) < p.metab) {
-		p.food--;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Pet {
+	pub t: PetType,
+	pub born: u64,
+	pub food: i8,
+	pub metabolism: u8,
+	pub training: i8,
+	pub discipline: u8,
+	// At first we want to just delete the entry when we die but we
+	// might want to reincarnate later.
+	pub alive: bool,
+}
+
+pub fn tick(rng: &mut impl Rng, p: &mut Pet) {
+	if rng.random_range(0..100) < p.metabolism {
+		p.food -= 1;
 	}
 
-	if (rng::random_range(0..100) < p.atten) {
-		p.fun--;
+	if rng.random_range(0..100) < p.discipline {
+		p.training -= 1;
 	}
 
-	p.alive = (p.fun > -5) && (p.food > -5);
+	p.alive = (p.training > -5) && (p.food > -5);
 }
